db6c373de0367afe5f1a43ea99b64854e319cd78
Venkat Yadav <Venkat Yadav@LAPTOP-0U0O4UUF>
2021-06-10 20:37:59 +0530

    my second commit

---
+++ Lab5/src/main/java/Lab5/AgeInvalidException.java
@@ -0,0 +1,11 @@
+package Lab5;
+
+
+
+public class AgeInvalidException extends RuntimeException {
+
+	public String toString()
+	{
+		return"com.Lab5.AgeInvalidException:Given age is not vaild to accept";
+	}
+}
---
+++ Lab5/src/main/java/Lab5/EmployeeException.java
@@ -0,0 +1,14 @@
+package Lab5;
+
+
+
+public class EmployeeException extends RuntimeException {
+
+	public String toString()
+	{
+		return"Lab5.EmployeeException:Salary is lessthan 3000";
+	}
+	
+	
+
+}
